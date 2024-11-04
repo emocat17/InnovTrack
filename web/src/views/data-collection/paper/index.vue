4ec19e8cf2c2624985f24c48d9664ddebd7ed0b6
emocat17 <976856169@qq.com>
2024-11-04 20:54:06 +0800

    Update index.vue

--- web/src/views/data-collection/paper/index.vue
+++ web/src/views/data-collection/paper/index.vue
@@ -84,3 +84,38 @@ export default {
   font-size: 27px;
 }
 </style>
+
+
+<!-- <template>
+  <div>
+    <input v-model="keyword" placeholder="输入关键字" />
+    <button @click="fetchData">下载论文</button>
+    <div v-if="message">{{ message }}</div>
+  </div>
+</template>
+
+<script>
+import api from '@/api'
+
+export default {
+  data() {
+    return {
+      keyword: '',
+      message: '',
+    }
+  },
+
+  methods: {
+    async fetchData() {
+      this.message = 正在下载与"${this.keyword}"相关的论文...
+      try {
+        await api.fetchArxivData(this.keyword)
+        this.message = 下载完成！
+      } catch (error) {
+        console.error('下载失败', error)
+        this.message = '下载失败，请重试。'
+      }
+    },
+  },
+}
+</script> -->
