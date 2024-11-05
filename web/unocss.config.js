ab4ff87e0054fe7ac10c079b955dc8b44cdbfc7b
emocat17 <976856169@qq.com>
2024-11-05 13:04:39 +0800

    Update unocss.config.js
    修改pnpm dev报错问题:
    WARN  [unocss] exclude option is deprecated, use content.pipeline.exclude instead.

--- web/unocss.config.js
+++ web/unocss.config.js
@@ -1,18 +1,35 @@
 import { defineConfig, presetAttributify, presetUno } from 'unocss'
 
 export default defineConfig({
-  exclude: [
-    'node_modules',
-    '.git',
-    '.github',
-    '.husky',
-    '.vscode',
-    'build',
-    'dist',
-    'mock',
-    'public',
-    './stats.html',
-  ],
+  // exclude: [
+  //   'node_modules',
+  //   '.git',
+  //   '.github',
+  //   '.husky',
+  //   '.vscode',
+  //   'build',
+  //   'dist',
+  //   'mock',
+  //   'public',
+  //   './stats.html',
+  // ],
+  content:{
+    pipeline:{
+      exclude: [
+        'node_modules',
+        '.git',
+        '.github',
+        '.husky',
+        '.vscode',
+        'build',
+        'dist',
+        'mock',
+        'public',
+        './stats.html',
+      ],
+    }
+  },//修改为content.pipeline.exclude形式,不然运行pnpm dev会报错:
+  // WARN  [unocss] exclude option is deprecated, use content.pipeline.exclude instead. 
   presets: [presetUno(), presetAttributify()],
   shortcuts: [
     ['wh-full', 'w-full h-full'],
