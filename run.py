90404e1f7941d136fa96beedfe02a1852db4733e
emocat <97672902+emocat17@users.noreply.github.com>
2025-03-05 20:39:17 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,19 +1,4 @@
-# import uvicorn
-
-# if __name__ == "__main__":
-#     uvicorn.run(
-#         "app:app",
-#         host="0.0.0.0",
-#         port=9999,
-#         reload=True,
-#         log_config="uvicorn_loggin_config.json"
-#     )
-
-import asyncio
 import uvicorn
-
-# 为 Windows 设置事件循环策略
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 if __name__ == "__main__":
     uvicorn.run(
@@ -21,6 +6,21 @@ if __name__ == "__main__":
         host="0.0.0.0",
         port=9999,
         reload=True,
-        log_config="uvicorn_loggin_config.json",
-        loop="asyncio"  # 设置为原生 asyncio 事件循环
+        log_config="uvicorn_loggin_config.json"
     )
+
+# import asyncio
+# import uvicorn
+
+# # 为 Windows 设置事件循环策略
+# asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+# if __name__ == "__main__":
+#     uvicorn.run(
+#         "app:app",
+#         host="0.0.0.0",
+#         port=9999,
+#         reload=True,
+#         log_config="uvicorn_loggin_config.json",
+#         loop="asyncio"  # 设置为原生 asyncio 事件循环
+#     )
